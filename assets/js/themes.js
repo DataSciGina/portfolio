b4b356697b89177fffd45f038c15ca07ec98b799
DataSciGina <fernandezagostina.ds@gmail.com>
2026-01-07 19:30:13 -0300

    auto theme recognition and storage of preference

--- assets/js/themes.js
+++ assets/js/themes.js
@@ -1,6 +1,18 @@
 const darkMode = document.querySelector('.dark-mode-toggle');
 const body = document.body;
+const savedTheme = localStorage.getItem('theme');
+
+if (!savedTheme) {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        body.classList.add('dark');
+    }
+
+    localStorage.setItem('theme', 'dark');
+} else if (savedTheme === 'dark') {
+    body.classList.add('dark');
+}
 
 darkMode.addEventListener('click', () => {
     body.classList.toggle('dark');
+    localStorage.setItem('theme', body.classList.contains('dark') ? 'dark' : 'light');
 });
